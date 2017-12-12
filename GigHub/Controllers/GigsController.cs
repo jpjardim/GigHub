cf8b9c82334702610a1cd559279589e7c7ae58a3
Paulo Jardim <jopanjardim@gmail.com>
2017-12-12 17:00:18 +0000

    Added ViewModel / Added the fields on the view

---
+++ GigHub/Controllers/GigsController.cs
@@ -0,0 +1,32 @@
+﻿using GigHub.Models;
+using GigHub.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class GigsController : Controller
+    {
+        // Create the default DbContext
+        private ApplicationDbContext _context;
+
+        //Initialize the DbContext on the constructor
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Gigs
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel
+            {
+                //Set the Genres to the models and pass it to list
+                Genres = _context.Genres.ToList()
+            };
+
+            //return the view model instead of the view
+            return View(viewModel);
+        }
+    }
+}
